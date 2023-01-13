d80b93e48ef09adcffa79f636332a3cb2ba36b20
mambasa <kal990574@gmail.com>
2023-01-13 17:58:45 +0900

    Update bj-9009-fibonacci.py

--- greedy/bj-9009-fibonacci.py
+++ greedy/bj-9009-fibonacci.py
@@ -1,19 +1,15 @@
-arr =[]
 j=0
-result =[]
-def fibo(n):
-    if n in (1,2):
-        return 1
-    return fibo(n-1) + fibo(n-2)
-for i in range(1,50):
-    arr.append(fibo(i))
+fibo = [0,1]
+for i in range(2,50):
+    fibo.append(fibo[i-2] + fibo[i-1])
 N=int(input())
 for i in range(N):
+    result =[]
     j = int(input())
     while(j):
         for i in range(50):
-            if(fibo(i) <= j):
-                m = fibo(i)
+            if(fibo[i] <= j):
+                m = fibo[i]
         j -= m
         result.append(m)
         result.sort()
