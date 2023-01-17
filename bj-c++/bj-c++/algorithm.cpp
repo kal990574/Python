ae662e2c76b3c796fec3c6c179f4ccefacaf631b
mambasa <kal990574@gmail.com>
2023-01-17 17:26:00 +0900

    1330 clear

--- bj-c++/bj-c++/algorithm.cpp
+++ bj-c++/bj-c++/algorithm.cpp
@@ -2,26 +2,10 @@
 #include <string>
 using namespace std;
 int main() {
-	string str;
-	getline(cin, str);
-	int max = 0;
-	int cnt = 0;
-	int check = 0;
-	int arr[26] ={0,}; // 0~25, 대문자 차이 32
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] > 96) arr[str[i] - 97]++;
-		else arr[str[i] - 65]++;
-	}
-	for (int i = 0; i < 26; i++) {
-		if (max < arr[i]) {
-			max = arr[i];
-			check = i;
-		}
-	}
-	for (int i = 0; i < 26; i++) {
-		if (max == arr[i]) cnt++;
-	}
-	if (cnt > 1) cout << "?";
-	else cout << char(check + 65);
+	int A, B;
+	cin >> A >> B;
+	if (A < B) cout << "<";
+	else if (A > B) cout << ">";
+	else cout << "==";
 }
-//1001 1008 1152 1157
+//1001 1008 1152 1157 1330
