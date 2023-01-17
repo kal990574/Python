8c70e605b0b0f16465875ea2f4b80a3cda6c1ea3
mambasa <kal990574@gmail.com>
2023-01-17 17:18:56 +0900

    1157 clear

--- bj-c++/bj-c++/algorithm.cpp
+++ bj-c++/bj-c++/algorithm.cpp
@@ -3,14 +3,25 @@
 using namespace std;
 int main() {
 	string str;
-	int cnt = 1;
 	getline(cin, str);
-	for (int i = 0; i < str.length(); i++) 
-	{
-		if (str[i] == ' ') cnt++;
+	int max = 0;
+	int cnt = 0;
+	int check = 0;
+	int arr[26] ={0,}; // 0~25, 대문자 차이 32
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] > 96) arr[str[i] - 97]++;
+		else arr[str[i] - 65]++;
 	}
-	if (str[0] == ' ') cnt--;
-	if (str[str.length() - 1] == ' ') cnt--;
-	cout << cnt;
+	for (int i = 0; i < 26; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+			check = i;
+		}
+	}
+	for (int i = 0; i < 26; i++) {
+		if (max == arr[i]) cnt++;
+	}
+	if (cnt > 1) cout << "?";
+	else cout << char(check + 65);
 }
 //1001 1008 1152 1157
