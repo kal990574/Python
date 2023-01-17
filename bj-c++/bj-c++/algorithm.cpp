525de939610b4c2dbab5681e97be4ff5295f4d6f
mambasa <kal990574@gmail.com>
2023-01-17 17:43:41 +0900

    1546 clear

--- bj-c++/bj-c++/algorithm.cpp
+++ bj-c++/bj-c++/algorithm.cpp
@@ -2,10 +2,22 @@
 #include <string>
 using namespace std;
 int main() {
-	int A, B;
-	cin >> A >> B;
-	if (A < B) cout << "<";
-	else if (A > B) cout << ">";
-	else cout << "==";
+	int cnt = 0;
+	float max = 0;
+	float res = 0;
+	cin >> cnt;
+	float* arr = new float[cnt];
+	for (int i = 0; i < cnt; i++) {
+		cin >> arr[i];
+		if (max < arr[i]) max = arr[i];
+	}
+	for (int i = 0; i < cnt; i++) {
+		arr[i] = 100 * arr[i] / max;
+	}
+	for (int i = 0; i < cnt; i++) {
+		res += arr[i];
+	}
+	res = res / cnt;
+	cout << res;
 }
-//1001 1008 1152 1157 1330
+//1001 1008 1152 1157 1330 1546
